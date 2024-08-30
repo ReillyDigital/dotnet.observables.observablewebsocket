27f5a3347574506e893d6ed2cd9821e7ea12106f
Andrew Reilly <andrewdrx@live.com>
2024-08-30 08:37:45 -0400

    Working on moving library to its own package.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ReillyDigital.Observables.ObservableWebSocket
 
 An observable WebSocket library for .NET for creating WebSockets that have subscribable events.
+
+## Usage
+
+
+
+## Links
+
+Sample Project:
+https://gitlab.com/reilly-digital/dotnet/observables.observablewebsocket/-/tree/main/src/Observables.ObservableWebSocket.Sample
+
+NuGet:
+https://www.nuget.org/packages/ReillyDigital.Observables.ObservableWebSocket
--- src/Observables.ObservableWebSocket/Observables/ObservableWebSocket.cs
+++ src/Observables.ObservableWebSocket/Observables/ObservableWebSocket.cs
@@ -2,34 +2,76 @@ namespace ReillyDigital.Observables;
 
 using System.Net.WebSockets;
 
+/// <summary>
+/// Observable wrapper for a websocket connection. Subscribable events are provided for when data is received or sent,
+/// as well as events for when the connection is aborted, closed or disposed.
+/// </summary>
 public class ObservableWebSocket : IDisposable
 {
+	/// <summary>
+	/// An event triggered when the websocket connection is aborted.
+	/// </summary>
 	public event EventHandler<EventArgs>? Aborted;
 
+	/// <summary>
+	/// An event triggered when binary data is sent over the websocket connection.
+	/// </summary>
 	public event EventHandler<ReadOnlyMemory<byte>>? BinarySent;
 
+	/// <summary>
+	/// An event triggered when the websocket connection is closed.
+	/// </summary>
 	public event EventHandler<EventArgs>? Closed;
 
+	/// <summary>
+	/// An event triggered when an output closure message is sent over the websocket connection.
+	/// </summary>
 	public event EventHandler<EventArgs>? CloseSent;
 
+	/// <summary>
+	/// An event triggered when the websocket connection is disposed.
+	/// </summary>
 	public event EventHandler<EventArgs>? Disposed;
 
+	/// <summary>
+	/// An event triggered when text data is sent over the websocket connection.
+	/// </summary>
 	public event EventHandler<ReadOnlyMemory<byte>>? MessageSent;
 
-	public event EventHandler<EventArgs>? OutputClosed;
-
+	/// <summary>
+	/// An event triggered when data is received over the websocket connection.
+	/// </summary>
 	public event EventHandler<ReadOnlyMemory<byte>>? Received;
 
+	/// <summary>
+	/// Indicates the reason why the remote endpoint initiated the close handshake.
+	/// </summary>
 	public WebSocketCloseStatus? CloseStatus => WebSocket.CloseStatus;
 
+	/// <summary>
+	/// Allows the remote endpoint to describe the reason why the connection was closed.
+	/// </summary>
 	public string? CloseStatusDescription => WebSocket.CloseStatusDescription;
 
+	/// <summary>
+	/// Returns the current state of the WebSocket connection.
+	/// </summary>
 	public WebSocketState State => WebSocket.State;
 
+	/// <summary>
+	/// Gets the subprotocol that was negotiated during the opening handshake.
+	/// </summary>
 	public string? SubProtocol => WebSocket.SubProtocol;
 
+	/// <summary>
+	/// The wrapped web socket connection.
+	/// </summary>
 	private WebSocket WebSocket { get; }
 
+	/// <summary>
+	/// Constructor for this class given an existing websocket connection to wrap.
+	/// </summary>
+	/// <param name="webSocket">The websocket connection to be wrapped.</param>
 	public ObservableWebSocket(WebSocket webSocket)
 	{
 		if (webSocket.State != WebSocketState.Open)
@@ -41,6 +83,10 @@ public class ObservableWebSocket : IDisposable
 		WebSocket = webSocket;
 	}
 
+	/// <summary>
+	/// Constructor for this class given a URI to use for a new websocket connection to wrap.
+	/// </summary>
+	/// <param name="uri">The URI for a new websocket connection to wrap.</param>
 	public ObservableWebSocket(string uri) : this(new Uri(uri)) { }
 
 	public ObservableWebSocket(Uri uri)
@@ -50,30 +96,34 @@ public class ObservableWebSocket : IDisposable
 		WebSocket = clientWebSocket;
 	}
 
+	/// <summary>
+	/// Aborts the wrapped websocket connection and invokes the <see cref="Aborted" /> event.
+	/// </summary>
 	public void Abort()
 	{
 		WebSocket.Abort();
 		Aborted?.Invoke(this, new());
 	}
 
+	/// <summary>
+	/// Closes the wrapped websocket connection and invokes the <see cref="Closed" /> event.
+	/// </summary>
+	/// <param name="status">The status of the closure.</param>
+	/// <param name="description">The description of the closure.</param>
+	/// <param name="cancellationToken">A cancellation token for the async request.</param>
+	/// <exception cref="OperationCanceledException" />
 	public async Task CloseAsync(
-		WebSocketCloseStatus closeStatus, string? statusDescription, CancellationToken cancellationToken
+		WebSocketCloseStatus status, string? description, CancellationToken cancellationToken = default!
 	)
 	{
 		cancellationToken.ThrowIfCancellationRequested();
-		await WebSocket.CloseAsync(closeStatus, statusDescription, cancellationToken);
+		await WebSocket.CloseAsync(status, description, cancellationToken);
 		Closed?.Invoke(this, new());
 	}
 
-	public async Task CloseOutputAsync(
-		WebSocketCloseStatus closeStatus, string? statusDescription, CancellationToken cancellationToken
-	)
-	{
-		cancellationToken.ThrowIfCancellationRequested();
-		await WebSocket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
-		OutputClosed?.Invoke(this, new());
-	}
-
+	/// <summary>
+	/// Disposes the wrapped websocket connection and invokes the <see cref="Disposed" /> event.
+	/// </summary>
 	public void Dispose()
 	{
 		WebSocket.Dispose();
@@ -114,10 +164,15 @@ public class ObservableWebSocket : IDisposable
 		BinarySent?.Invoke(this, data);
 	}
 
-	public async Task SendCloseAsync(CancellationToken cancellationToken)
+	public async Task SendCloseAsync(CancellationToken cancellationToken) =>
+		await SendCloseAsync(WebSocketCloseStatus.NormalClosure, null, cancellationToken);
+
+	public async Task SendCloseAsync(
+		WebSocketCloseStatus closeStatus, string? statusDescription, CancellationToken cancellationToken
+	)
 	{
 		cancellationToken.ThrowIfCancellationRequested();
-		await WebSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, null, cancellationToken);
+		await WebSocket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
 		CloseSent?.Invoke(this, new());
 	}
 
