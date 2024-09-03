674dea8f0ae144c7ecf41b6d4ff0fcca354af076
Andrew Reilly <andrewdrx@live.com>
2024-09-02 22:17:37 -0400

    Still working on sample.

--- src/Observables.ObservableWebSocket.Sample/Program.cs
+++ src/Observables.ObservableWebSocket.Sample/Program.cs
@@ -1,31 +1,37 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
-System.Net.WebSockets.WebSocket? serverSocket = null;
 var app = WebApplication.CreateBuilder(args).Build();
 app.UseWebSockets();
 app.Map(
 	"",
 	async (context) =>
-		serverSocket = await context.WebSockets.AcceptWebSocketAsync()
+	{
+		using var socket = await context.WebSockets.AcceptWebSocketAsync();
+		await socket.SendAsync(
+			System.Text.Encoding.UTF8.GetBytes("Hello World!"),
+			System.Net.WebSockets.WebSocketMessageType.Text,
+			endOfMessage: true,
+			cancellationToken: new()
+		);
+		await Task.Delay(5000);
+	}
 );
 app.Start();
 
-var observableWebSocket =
-	new ObservableWebSocket(app.Urls.First().Replace("http://", "ws://"));
+//var observableWebSocket =
+//	new ObservableWebSocket(app.Urls.First().Replace("http://", "ws://"));
+//observableWebSocket.Received +=
+//	(object? sender, ObservableWebSocketData data) =>
+//		Console.WriteLine(System.Text.Encoding.UTF8.GetString(data.Bytes.Span));
+//observableWebSocket.ListenAsync();
 
-observableWebSocket.Received +=
-	(object? sender, ObservableWebSocketData data) =>
-		Console.WriteLine(System.Text.Encoding.UTF8.GetString(data.Bytes.Span));
-observableWebSocket.ListenAsync();
-Task.Delay(1000).Wait();
-
-serverSocket?
-	.SendAsync(
-		System.Text.Encoding.UTF8.GetBytes("Hello World!"),
-		System.Net.WebSockets.WebSocketMessageType.Text,
-		true,
-		new CancellationToken()
-	)
-	.Wait();
-Task.Delay(1000).Wait();
+await Task.Run(
+	async () =>
+	{
+		using var socket = new System.Net.WebSockets.ClientWebSocket();
+		await socket.ConnectAsync(new Uri(app.Urls.First().Replace("http://", "ws://")), new());
+		var buffer = new Memory<byte>(new byte[2048]);
+		await socket.ReceiveAsync(buffer, new());
+	}
+);
